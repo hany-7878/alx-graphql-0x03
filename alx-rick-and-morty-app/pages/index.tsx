09b5fbde15686720d73599eecb635668564ad1e7
hany-7878 <htesfaye641@gmail.com>
2026-01-03 21:44:32 +0300

    Initial commit for alx-graphql-0x03

--- alx-rick-and-morty-app/pages/index.tsx
+++ alx-rick-and-morty-app/pages/index.tsx
@@ -1,4 +1,5 @@
-import { useQuery } from "@apollo/client"
+import { useQuery } from "@apollo/client/react"
+
 import { GET_EPISODES } from "@/graphql/queries"
 import { EpisodeProps } from "@/interfaces"
 import EpisodeCard from "@/components/common/EpisodeCard"
